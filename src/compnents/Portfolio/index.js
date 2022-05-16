d47946fc87df3db20d0611d82d64ad43c3b1c766
ulisMac <rosas_u@ymail.com>
2022-05-16 02:14:03 -0700

    finished portfolio component

--- src/compnents/Portfolio/index.js
+++ src/compnents/Portfolio/index.js
@@ -9,67 +9,61 @@ function Portfolio() {
             <div className="row ms-1">
                 {/* portfolio item */}
                 <div class="col-sm-6 mt-4">
-                    <div className="card" style={{ width: '18rem;' }}>
+                    <div className="card bg-dark text-light" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <img src="../../assets/images/6feetAwayWideSized.jpg" className="card-img-top" alt="project"/>
+                            <h5 className="card-title">6FeetAwayGaming</h5>
+                            <a href="https://github.com/UlisesRosas/6-FeetAway-Gaming" className="btn btn-secondary">Go To</a>
                         </div>
                     </div>
                 </div>
                 {/* portfolio item */}
                 <div class="col-sm-6 mt-4">
-                    <div className="card" style={{ width: '18rem;' }}>
+                    <div className="card bg-dark text-light" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <img src="../../assets/images/only-friends.jpg" className="card-img-top" alt="project"/>
+                            <h5 className="card-title">Only_Friends</h5>
+                            <a href="https://github.com/UlisesRosas/Only_Friends" className="btn btn-secondary">Go To</a>
                         </div>
                     </div>
                 </div>
                 {/* portfolio item */}
                 <div class="col-sm-6 mt-4">
-                    <div className="card" style={{ width: '18rem;' }}>
+                    <div className="card bg-dark text-light" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <img src="../../assets/images/item2-image.jpg" className="card-img-top" alt="project"/>
+                            <h5 className="card-title">Filler Project</h5>
+                            <a href="#" className="btn btn-secondary">Go To</a>
                         </div>
                     </div>
                 </div>
                 {/* portfolio item */}
                 <div class="col-sm-6 mt-4">
-                    <div className="card" style={{ width: '18rem;' }}>
+                    <div className="card bg-dark text-light" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <img src="../../assets/images/item4-image.jpg" className="card-img-top" alt="project"/>
+                            <h5 className="card-title">Filler Project</h5>
+                            <a href="#" className="btn btn-secondary">Go To</a>
                         </div>
                     </div>
                 </div>
                 {/* portfolio item */}
                 <div class="col-sm-6 mt-4">
-                    <div className="card" style={{ width: '18rem;' }}>
+                    <div className="card bg-dark text-light" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <img src="../../assets/images/item5-image.jpg" className="card-img-top" alt="project"/>
+                            <h5 className="card-title">Filler Project</h5>
+                            <a href="#" className="btn btn-secondary">Go To</a>
                         </div>
                     </div>
                 </div>
                 {/* portfolio item */}
                 <div class="col-sm-6 mt-4">
-                    <div className="card" style={{ width: '18rem;' }}>
+                    <div className="card bg-dark text-light" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <img src="../../assets/images/item2-image.jpg" className="card-img-top" alt="project"/>
+                            <h5 className="card-title">Filler Project</h5>
+                            <a href="#" className="btn btn-secondary">Go To</a>
                         </div>
                     </div>
                 </div>
