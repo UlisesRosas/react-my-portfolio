3173e8e04bc5be89c8ebc1e04cc425afcbd837ab
SLEEPlESS <rosas_u@ymail.com>
2022-05-18 18:45:46 -0700

    added deploy dependencies

--- src/compnents/Portfolio/index.js
+++ src/compnents/Portfolio/index.js
@@ -44,8 +44,7 @@ function Portfolio() {
                 <div class="col-sm-6 mt-4">
                     <div className="card" style={{ width: '18rem;' }}>
                         <div className="card-body">
-                            <img src="..." className="card-img-top" alt="..."/>
-                            <h5 className="card-title">Special title treatment</h5>
+cd                            <h5 className="card-title">Special title treatment</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
