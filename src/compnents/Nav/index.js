8612bb9f76b823962625ce6444120ea881e6e174
ulisMac <rosas_u@ymail.com>
2022-06-29 23:56:43 -0700

    changing the nav bar to MUI

--- src/compnents/Nav/index.js
+++ src/compnents/Nav/index.js
@@ -1,45 +1,210 @@
 
-import React from 'react';
-// import bootstrap for react 'check bootstrap react docs'
+// import React from 'react';
+// // import bootstrap for react 'check bootstrap react docs'
 
 
-// use props to pass down the page array from App
-function Nav(props) {
-  //to see what gets passed down in the props
-  console.log(props);
+// // use props to pass down the page array from App
+// function Nav(props) {
+//   //to see what gets passed down in the props
+//   console.log(props);
 
 
-  const {
-    pages = [],
-    currentPage,
-    setCurrentPage,
+//   const {
+//     pages = [],
+//     currentPage,
+//     setCurrentPage,
 
-  } = props
+//   } = props
 
+
+//   return (
+
+//     <nav className="row navbar navbar-expand-lg navbar-dark bg-dark">
+//       <div className="container-fluid">
+//         <h1 className="navbar-brand ms-2">Ulises Rosas</h1>
+//         {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon"></span>
+//         </button> */}
+//         <div className="navbar-collapse flex-grow-0" id="navbarNav">
+//           <ul className="navbar-nav justify-content-end">
+//             {/* passed in index to be able to create a li for every item in the pages array */}
+//             {props.pages.map((page) => (
+//               <li className="nav-item m-1 nav-link"
+//                 key={page.name}>
+//                 {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+//                 <span onClick={() => setCurrentPage(page.name)}>{page.name}</span>
+//               </li>))}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+
+// }
+
+// export default Nav;
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Ulises Rosas
+          </Typography>
 
-    <nav className="row navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <h1 className="navbar-brand ms-2">Ulises Rosas</h1>
-        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
-        <div className="navbar-collapse flex-grow-0" id="navbarNav">
-          <ul className="navbar-nav justify-content-end">
-            {/* passed in index to be able to create a li for every item in the pages array */}
-            {props.pages.map((page) => (
-              <li className="nav-item m-1 nav-link"
-                key={page.name}>
-                {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
-                <span onClick={() => setCurrentPage(page.name)}>{page.name}</span>
-              </li>))}
-          </ul>
-        </div>
-      </div>
-    </nav>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {/* maps through each pages array item */}
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            {/* <Tooltip title="Open settings"> */}
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            {/* </Tooltip> */}
+            {/* <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            > */}
+              {/* display the menu items from settings array */}
+              {/* {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))} */}
+            {/* </Menu> */}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-
-}
-
-export default Nav;
+};
+export default ResponsiveAppBar;
